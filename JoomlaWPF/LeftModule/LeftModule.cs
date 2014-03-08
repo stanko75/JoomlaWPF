7d89258ffd72e241a2692ac89ace31160c940951
stanko75 <stanko@milosev.com>
2014-03-08 10:49:34 +0100

    #2 Tabs added

--- JoomlaWPF/LeftModule/LeftModule.cs
+++ JoomlaWPF/LeftModule/LeftModule.cs
@@ -1,14 +1,18 @@
-﻿using Microsoft.Practices.Prism.MefExtensions.Modularity;
+﻿using System.ComponentModel.Composition;
+using Microsoft.Practices.Prism.MefExtensions.Modularity;
 using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
 
 namespace LeftModule
 {
   [ModuleExport(typeof(LeftModule))]
   public class LeftModule: IModule
   {
+    [Import]
+    public IRegionManager Region { get; set; }
     public void Initialize()
     {
-      
+      Region.RequestNavigate("LeftRegion", "LeftView");
     }
   }
 }
---
+++ JoomlaWPF/LeftModule/View/LeftView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Controls;
+using Microsoft.Practices.Prism.Regions;
+
+namespace LeftModule.View
+{
+  [Export("LeftView")]
+  [PartCreationPolicy(CreationPolicy.NonShared)]
+  [RegionMemberLifetime(KeepAlive = false)]
+  public partial class LeftView : UserControl
+  {
+    [ImportingConstructor]
+    public LeftView()
+    {
+      InitializeComponent();
+    }
+  }
+}
