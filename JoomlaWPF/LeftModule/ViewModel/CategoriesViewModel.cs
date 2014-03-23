3fa7da25e74ae3024c25c4939ea6a8a4aae6ac2b
stanko75 <stanko@milosev.com>
2014-03-23 10:40:32 +0100

    #2 Connection string moved to app.config

--- JoomlaWPF/LeftModule/ViewModel/CategoriesViewModel.cs
+++ JoomlaWPF/LeftModule/ViewModel/CategoriesViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.Configuration;
 using LeftModule.Model;
 using MySql.Data.MySqlClient;
 
@@ -8,6 +9,7 @@ namespace LeftModule.ViewModel
   [Export]
   public class CategoriesViewModel
   {
+    private string JoomlaConStr = ConfigurationManager.ConnectionStrings["JoomlaCon"].ConnectionString;
     public List<ICategory> CategoriesList
     {
       get { return m_categories; }
@@ -25,13 +27,7 @@ namespace LeftModule.ViewModel
     {
       m_categories = new List<ICategory>();
 
-      string MyConString =
-"SERVER=myServer;" +
-"DATABASE=myDb;" +
-"UID=myUid;" +
-"PASSWORD=myPass;Convert Zero Datetime=True";
-
-      var connection = new MySqlConnection(MyConString);
+      var connection = new MySqlConnection(JoomlaConStr);
 
       string sql = "select * from jos_categories where level = 0 order by id ";
 
@@ -63,13 +59,8 @@ namespace LeftModule.ViewModel
       foreach (ICategory category in CategoriesList)
       {
         i++;
-        string MyConString =
-"SERVER=myServer;" +
-"DATABASE=myDb;" +
-"UID=myUid;" +
-"PASSWORD=myPass;Convert Zero Datetime=True";
 
-        var connection = new MySqlConnection(MyConString);
+        var connection = new MySqlConnection(JoomlaConStr);
 
         string sql = "select * from jos_categories where parent_id = " + category.Id + " order by id ";
 
