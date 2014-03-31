a9aba1e7df7a491be8170e8dfb50fc3154076a92
stanko75 <stanko@milosev.com>
2014-03-31 21:53:23 +0200

    Added missing files

---
+++ JoomlaWPF/LeftModule/Model/TreeCategoriesModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace LeftModule.Model
+{
+  public class TreeCategoriesModel: ITreeCategory
+  {
+    private readonly List<ITreeCategory> m_categories;
+    public TreeCategoriesModel()
+    {
+      m_categories = new List<ITreeCategory>();
+    }
+    public string Name { get; set; }
+    public int Id { get; set; }
+
+    public List<ITreeCategory> Categories
+    {
+      get { return m_categories; }
+    }
+  }
+}
---
+++ JoomlaWPF/LeftModule/ViewModel/CategoriesTreeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Composition;
+
+namespace LeftModule.ViewModel
+{
+  using System.Collections.Generic;
+  using Model;
+
+  [Export]
+  public class CategoriesTreeViewModel
+  {
+    public List<ITreeCategory> CategoriesList { get; set; }
+
+    [ImportingConstructor]
+    public CategoriesTreeViewModel(ICategoriesTreeGetter categoriesTreeGetter)
+    {
+      CategoriesList = categoriesTreeGetter.GetCategoriesInTree();
+    }
+  }
+}
